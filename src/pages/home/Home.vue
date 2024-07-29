510be27a27006ebf307550ea6f8b9f83d64522db
kaylyn <lrlkaylyn@gmail.com>
2024-07-30 08:35:25 +1200

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div class="home">home</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
-#app {
-}
+
 </style>
