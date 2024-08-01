1a0de22ee1859f05e0849c518f6f11bbb88cc229
kaylyn <lrlkaylyn@gmail.com>
2024-08-01 12:18:11 +1200

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://pic5.sucaisucai.com/13/24/13524735_2.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://png.pngtree.com/png-clipart/20221230/original/pngtree-banner-template-with-asia-travel-concept-design-for-advertise-and-marketing-png-image_8830002.png'
+      }]
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
