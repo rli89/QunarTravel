46873b9962cd952ea88dbd7657f476a4a0eac02e
kaylyn <lrlkaylyn@gmail.com>
2024-08-26 09:14:11 +1200

    location

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -7,6 +7,7 @@
     ></detail-banner>
     <detail-header></detail-header>
     <div class="content">
+      <detail-location></detail-location>
       <detail-list :list="list"></detail-list>
     </div>
   </div>
@@ -16,13 +17,15 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import DetailLocation from './components/Location'
 import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
     DetailHeader,
-    DetailList
+    DetailList,
+    DetailLocation
   },
   data () {
     return {
---
+++ src/pages/detail/components/Location.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+  <div class="location">
+    <div class="one border-bottom">
+        <span class="location-icon" ></span>
+        辽宁省大连市沙河口区中山路608-6-8号(星海 ...
+    </div>
+    <div class="two border-bottom">
+        <span class="time-icon"></span>
+        查看景点简介及开放时间
+    </div>
+    <div class="three border-bottom">
+      <span class="review-icon"></span>
+      4.9分
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DetailLocation',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.location
+  line-height: .8rem
+  font-size: .32rem
+  padding: 0 .2rem
+.location-icon
+  position: relative
+  left: .06rem
+  top: .06rem
+  display: inline-block
+  width: .36rem
+  height: .36rem
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -1.80rem no-repeat
+  margin-right: .1rem
+  background-size: .4rem 3rem
+.time-icon
+  position: relative
+  left: .06rem
+  top: .06rem
+  display: inline-block
+  width: .36rem
+  height: .36rem
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -2.20rem no-repeat
+  margin-right: .1rem
+  background-size: .4rem 3rem
+
+.review-icon
+  position: relative
+  left: .06rem
+  top: .06rem
+  display: inline-block
+  width: .36rem
+  height: .36rem
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -1.36rem no-repeat
+  margin-right: .1rem
+  background-size: .4rem 3rem
+</style>
